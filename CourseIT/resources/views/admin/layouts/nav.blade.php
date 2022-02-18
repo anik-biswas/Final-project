5edfcef85383a9577874f47de98e47b98798b505
anik-biswas <anik.biswas.an@gmail.com>
2022-02-18 12:46:35 +0600

    updae

---
+++ CourseIT/resources/views/admin/layouts/nav.blade.php
@@ -0,0 +1,46 @@
+<!-- Sidebar Menu -->
+<nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+      <!-- Add icons to the links using the .nav-icon class
+           with font-awesome or any other icon font library -->
+     {{-- <li class="nav-item">
+        <a href="#" class="nav-link">
+          <i class="nav-icon fas fa-tachometer-alt"></i>
+          <p>
+            Dashboard
+            <i class="right fas fa-angle-left"></i>
+          </p>
+        </a>
+        <ul class="nav nav-treeview">
+          <li class="nav-item">
+            <a href="/admin/index.html" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Dashboard v1</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="/admin/index2.html" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Dashboard v2</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="/admin/index3.html" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Dashboard v3</p>
+            </a>
+          </li>
+        </ul>
+      </li>--}}
+      <li class="nav-item">
+        <a href="/admin/iframe.html" class="nav-link">
+          <i class="nav-icon fas fa-ellipsis-h"></i>
+          <p>Tabbed IFrame Plugin</p>
+        </a>
+      </li>
+      
+    </ul>
+  </nav>
+  <!-- /.sidebar-menu -->
+
+<!-- /.sidebar -->
